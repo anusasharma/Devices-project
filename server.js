d8712bf07e9c411c47b7dd2a8357c08938725e7e
Anusa <anusoruban@gmail.com>
2022-03-06 15:07:55 +0530

    backend url

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/devices-project-frontend'));
+app.use(express.static('./dist/dist/devices-project'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/devices-project-frontend/'}),
+  res.sendFile('index.html', {root: 'dist/devices-project/'}),
 );
 
 // Start the app by listening on the default Heroku port
